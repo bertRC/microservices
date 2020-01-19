38ad7ac9849a63b34fc45963559946bf4eb30f6b
bertRC <sithalbert@ya.ru>
2020-01-19 13:14:26 +0300

    getAll and searchByName methods added

--- user-search-service/src/main/java/education/bert/service/UserSearchService.java
+++ user-search-service/src/main/java/education/bert/service/UserSearchService.java
@@ -3,8 +3,12 @@ package education.bert.service;
 import education.bert.jdbc.JdbcHelper;
 import education.bert.model.UserModel;
 
+import java.util.List;
+
 public class UserSearchService {
     private String dbUrl;
+
+    private final int minTextLengthForSearch = 3;
 
     public void setDbUrl(String dbUrl) {
         this.dbUrl = dbUrl;
@@ -24,5 +28,35 @@ public class UserSearchService {
                         resultSet.getString("name")
                 )
         ).orElse(null);
+    }
+
+    public List<UserModel> getAll() {
+        return JdbcHelper.executeQueryForList(
+                dbUrl,
+                "SELECT id, name FROM users;",
+                resultSet -> new UserModel(
+                        resultSet.getInt("id"),
+                        resultSet.getString("name")
+                )
+        );
+    }
+
+    public List<UserModel> searchByName(String name) {
+        if (name.length() < minTextLengthForSearch) {
+            throw new IllegalArgumentException("Text must contain at least " + minTextLengthForSearch + " characters");
+        }
+        return JdbcHelper.executeQueryForList(
+                dbUrl,
+                "SELECT id, name FROM users WHERE LOWER(name) LIKE ?;",
+                statement ->
+                {
+                    statement.setString(1, "%" + name.toLowerCase() + "%");
+                    return statement;
+                },
+                resultSet -> new UserModel(
+                        resultSet.getInt("id"),
+                        resultSet.getString("name")
+                )
+        );
     }
 }
