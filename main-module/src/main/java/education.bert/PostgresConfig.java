2fdfcfbc5503ce1f38cfa96736206216fb0363d4
bertRC <sithalbert@ya.ru>
2020-01-21 21:46:27 +0300

    PostgresConfig changed

--- main-module/src/main/java/education.bert/PostgresConfig.java
+++ main-module/src/main/java/education.bert/PostgresConfig.java
@@ -1,6 +1,6 @@
 package education.bert;
 
 public class PostgresConfig {
-    public final static String url = "jdbc:postgresql://192.168.99.100:32768/postgres?user=postgres";
-//    public final static String url = "jdbc:postgresql://localhost:5432/travis_ci_test?user=postgres";
+//    public final static String url = "jdbc:postgresql://192.168.99.100:32768/postgres?user=postgres";
+    public final static String url = "jdbc:postgresql://localhost:5432/travis_ci_test?user=postgres";
 }
