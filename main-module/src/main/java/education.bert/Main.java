7e669c2837b2341ad88cb603bbbc48d9a66cbd7b
bertRC <sithalbert@ya.ru>
2020-01-22 18:26:27 +0300

    some modification

--- main-module/src/main/java/education.bert/Main.java
+++ main-module/src/main/java/education.bert/Main.java
@@ -2,65 +2,37 @@ package education.bert;
 
 import education.bert.jdbc.JdbcHelper;
 import education.bert.model.UserModel;
-import education.bert.service.UserSearchService;
-import education.bert.service.UserUpdateService;
-import education.bert.service.VCService;
 
 import java.util.List;
-import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-//        final String dbUrl = PostgresConfig.url;
-//        final UserUpdateService userUpdateService = new UserUpdateService();
-//        final UserSearchService userSearchService = new UserSearchService();
-//
-//        userUpdateService.setDbUrl(dbUrl);
-//        userSearchService.setDbUrl(dbUrl);
-//
-//        JdbcHelper.executeUpdate(dbUrl, "DROP TABLE IF EXISTS users;");
-//        JdbcHelper.executeUpdate(dbUrl, "CREATE TABLE users (id SERIAL PRIMARY KEY, name TEXT NOT NULL);");
-//
-//        userUpdateService.saveUser(new UserModel(0, "Vasya"));
-//        userUpdateService.saveUser(new UserModel(0, "Petya"));
-//        userUpdateService.saveUser(new UserModel(0, "Sasha"));
-//        userUpdateService.saveUser(new UserModel(0, "Masha"));
-//
-//        int id = 3;
-//        final List<String> list = JdbcHelper.executeQueryForList(
-//                dbUrl,
-//                "EXPLAIN ANALYZE SELECT id, name FROM users WHERE id = ?;",
-//                statement ->
-//                {
-//                    statement.setInt(1, id);
-//                    return statement;
-//                },
-//                resultSet ->
-//                {
-//                    return resultSet.getString(1);
-//                }
-//        );
-//
-//        System.out.println(list.size());
-//        System.out.println(list);
+        final MyApplicationSmart app = new MyApplicationSmart();
 
-        final VCService vcService = new VCService();
+        app.setup();
+        app.userUpdateService.saveUser(new UserModel(0, "Vasya"));
+        app.userUpdateService.saveUser(new UserModel(0, "Petya"));
+        app.userUpdateService.saveUser(new UserModel(0, "Ivan"));
+        app.userUpdateService.saveUser(new UserModel(0, "Masha"));
+        app.userUpdateService.saveUser(new UserModel(0, "Sasha"));
+        app.userUpdateService.saveUser(new UserModel(0, "Dasha"));
+        app.userUpdateService.saveUser(new UserModel(0, "Bert"));
+        app.userUpdateService.saveUser(new UserModel(0, "Kolya"));
+        app.userUpdateService.saveUser(new UserModel(0, "Styopa"));
+        app.userUpdateService.saveUser(new UserModel(0, "Alesha"));
 
-        vcService.updateTableAndItem("users", "U1");
-        vcService.updateTableAndItem("users", "U2");
-        vcService.updateTableAndItem("users", "U1");
-//        vcService.updateTableAndItem("posts", "P1");
+        String name = "ash";
+        List<String> results = JdbcHelper.executeQueryForList(
+                PostgresConfig.url,
+                "EXPLAIN ANALYZE SELECT id, name FROM users WHERE LOWER(name) LIKE ?;",
+                statement ->
+                {
+                    statement.setString(1, "%" + name.toLowerCase() + "%");
+                    return statement;
+                },
+                resultSet -> resultSet.getString(1)
+        );
 
-        Map<Object, Integer> usersVersion = vcService.getItemsVersion("users");
-        System.out.println(usersVersion);
-
-        vcService.removeItemAndUpdateTable("users", "U2");
-        vcService.updateTableAndItem("users", "U1");
-
-        Map<Object, Integer> usersNewVersion = vcService.getItemsVersion("users");
-        System.out.println(usersNewVersion);
-
-        System.out.println(VCService.getChangedItems(usersNewVersion, usersVersion));
-        System.out.println(VCService.getMissedItems(usersNewVersion, usersVersion));
+        System.out.println(results);
     }
 }
